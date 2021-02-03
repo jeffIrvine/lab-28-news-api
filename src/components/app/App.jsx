70a04012eaaf0204e1ff8ec281e4120fae3636cf
Your NamejeffIrvine <jirvine1212@gmail.com>
2021-02-03 14:44:00 -0800

    displaying article comp on main page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Article from '../presentationalComp/Article';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Article title="Test" author="testAuth" description="testDesc" />;
 }
